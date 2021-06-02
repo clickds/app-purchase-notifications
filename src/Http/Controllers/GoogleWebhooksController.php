ac8fa106d48904978e849388a15f0fb9347caebf
James Doyley <jdoyley@gmail.com>
2021-06-02 10:43:34 +0100

    Update google webhooks controller

--- src/Http/Controllers/GoogleWebhooksController.php
+++ src/Http/Controllers/GoogleWebhooksController.php
@@ -6,6 +6,7 @@ use ClickDs\AppPurchaseNotifications\Models\GoogleNotification;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Log;
 use Throwable;
 
@@ -13,20 +14,25 @@ class GoogleWebhooksController extends Controller
 {
     public function store(Request $request): JsonResponse
     {
-        $notificationType = $this->calculateNotificationType($request);
+        if (!$request->has('message.data')) {
+            return new JsonResponse(null, 400);
+        }
+        $encryptedData = $request->input('message.data');
+        $jsonData = base64_decode($encryptedData);
+        $data = json_decode($jsonData, JSON_OBJECT_AS_ARRAY);
+        $notificationType = $this->calculateNotificationType($data);
 
         $notification = GoogleNotification::create([
             'type' => $notificationType,
-            'payload' => $request->all(),
+            'payload' => $data,
         ]);
 
         try {
             $this->dispatchJob($notification);
-            return new JsonResponse(null, 204);
         } catch (Throwable $e) {
             Log::error($e);
-            return new JsonResponse(null, 501);
         }
+        return new JsonResponse(null, 204);
     }
 
     private function dispatchJob(GoogleNotification $googleNotification): void
@@ -38,15 +44,18 @@ class GoogleWebhooksController extends Controller
         $jobClass::dispatch($googleNotification);
     }
 
-    private function calculateNotificationType(Request $request)
+    private function calculateNotificationType(array $data): string
     {
-        if (!$request->has('subscriptionNotification')) {
-            return 'Unknown';
-        }
-        $notificationType = $request->input('subscriptionNotification.notificationType');
-        if (array_key_exists($notificationType, GoogleNotification::GOOGLE_SUBSCRIPTION_NOTIFICATION_TYPES)) {
+        if (Arr::has($data, 'subscriptionNotification.notificationType')) {
+            $notificationType = Arr::get($data, 'subscriptionNotification.notifiationType');
             return GoogleNotification::GOOGLE_SUBSCRIPTION_NOTIFICATION_TYPES[$notificationType];
         }
-        return $notificationType;
+        if (Arr::has($data, 'oneTimeProductNotification')) {
+            return 'oneTimeProductNotification';
+        }
+        if (Arr::has($data, 'testNotification')) {
+            return 'testNotification';
+        }
+        return 'Unknown';
     }
 }

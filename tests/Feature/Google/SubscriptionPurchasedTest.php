3b9e1e7f4788a76e7406e6c48a98883f531f44c0
James Doyley <jdoyley@gmail.com>
2021-06-07 12:58:11 +0100

    Update tests to handle google notifications

--- tests/Feature/Google/SubscriptionPurchasedTest.php
+++ tests/Feature/Google/SubscriptionPurchasedTest.php
@@ -15,9 +15,7 @@ class SubscriptionPurchasedTest extends TestCase
 
     public function test_dispatches_configured_job(): void
     {
-        $payload = $this->payload();
-
-        $response = $this->makeRequest($payload);
+        $response = $this->makeRequest();
 
         $response->assertSuccessful();
         Queue::assertPushed(DummyGoogleJob::class);
@@ -25,24 +23,53 @@ class SubscriptionPurchasedTest extends TestCase
 
     public function test_stores_notification_payload(): void
     {
-        $payload = $this->payload();
-
-        $response = $this->makeRequest($payload);
+        $response = $this->makeRequest();
 
         $response->assertSuccessful();
         $this->assertDatabaseCount('google_notifications', 1);
+        $this->assertDatabaseHas('google_notifications', [
+            'type' => 'SUBSCRIPTION_PURCHASED',
+            'payload' => json_encode($this->payloadData()),
+        ]);
     }
 
-    private function makeRequest(array $payload): TestResponse
+    private function makeRequest(): TestResponse
     {
         $url = route('webhooks.google.store');
-        return $this->postJson($url, $payload);
+        $data = $this->requestData();
+        return $this->postJson($url, $data);
     }
 
-    private function payload(): array
+    private function payloadData(): array
     {
-        $data = GoogleNotification::factory()->definition();
-        $data['subscriptionNotification']['notificationType'] = 4;
+        return [
+            "version" => "1.0",
+            "packageName" => "com.app",
+            "eventTimeMillis" => "1622627834177",
+            "subscriptionNotification" => [
+                "version" => "1.0",
+                "notificationType" => 4,
+                "purchaseToken" => "some-purchase-token",
+                "subscriptionId" => 'com.app.subs.premium.monthly',
+            ]
+        ];
+    }
+
+    private function requestData(): array
+    {
+        $payloadData = $this->payloadData();
+
+        $jsonPayload = json_encode($payloadData);
+        $encodedData = base64_encode($jsonPayload);
+
+        $data = [
+            "message" => [
+                "data" => $encodedData,
+                "messageId" => "123",
+                "publishTime" => "2021-06-02T09:57:14.811Z"
+            ],
+            "subscription" => "projects/project-id/subscriptions/purchase-notifications-subscription"
+        ];
         return $data;
     }
 }
--- tests/Feature/Google/UnconfiguredJobTest.php
+++ tests/Feature/Google/UnconfiguredJobTest.php
@@ -23,24 +23,50 @@ class UnconfiguredJobTest extends TestCase
     public function test_dispatches_configured_job(): void
     {
         Log::shouldReceive('error')->once();
-        $payload = $this->payload();
 
-        $response = $this->makeRequest($payload);
+        $response = $this->makeRequest();
 
-        $response->assertStatus(501);
+        $response->assertStatus(204);
         Queue::assertNothingPushed();
     }
 
-    private function makeRequest(array $payload): TestResponse
+    private function makeRequest(): TestResponse
     {
         $url = route('webhooks.google.store');
-        return $this->postJson($url, $payload);
+        $data = $this->requestData();
+        return $this->postJson($url, $data);
     }
 
-    private function payload(): array
+    private function payloadData(): array
     {
-        $data = GoogleNotification::factory()->definition();
-        $data['subscriptionNotification']['notificationType'] = 4;
+        return [
+            "version" => "1.0",
+            "packageName" => "com.app",
+            "eventTimeMillis" => "1622627834177",
+            "subscriptionNotification" => [
+                "version" => "1.0",
+                "notificationType" => 4,
+                "purchaseToken" => "some-purchase-token",
+                "subscriptionId" => 'com.app.subs.premium.monthly',
+            ]
+        ];
+    }
+
+    private function requestData(): array
+    {
+        $payloadData = $this->payloadData();
+
+        $jsonPayload = json_encode($payloadData);
+        $encodedData = base64_encode($jsonPayload);
+
+        $data = [
+            "message" => [
+                "data" => $encodedData,
+                "messageId" => "123",
+                "publishTime" => "2021-06-02T09:57:14.811Z"
+            ],
+            "subscription" => "projects/project-id/subscriptions/purchase-notifications-subscription"
+        ];
         return $data;
     }
 }
